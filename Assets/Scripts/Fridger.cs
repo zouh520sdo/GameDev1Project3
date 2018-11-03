696254406df3a6be9782afe9f309b32c3a9ccc51
zouh520sdo <zouh520sdo@gmail.com>
2018-11-03 18:15:38 -0400

    Added script for interaction between player and interatable object

---
+++ Assets/Scripts/Fridger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fridger : Interactable {
+
+    public Animator fridgerAnimator;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I'm fridger.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Interact()
+    {
+        base.Interact();
+        fridgerAnimator.SetTrigger("Open");
+    }
+}
