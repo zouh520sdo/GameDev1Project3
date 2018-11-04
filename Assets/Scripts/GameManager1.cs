dabab605eed6bd447c3c572f5552bac79f25c1c5
Jessica Lichter <jlichter123@gmail.com>
2018-11-04 17:22:53 -0500

    working on path finding

---
+++ Assets/Scripts/GameManager1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager1 : MonoBehaviour {
+
+    public Transform girl;
+    public Transform bad_boy;
+    public Transform odd_boy;
+    public float gameTime = 360; //game is 6 minutes long, so 360 seconds 
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
