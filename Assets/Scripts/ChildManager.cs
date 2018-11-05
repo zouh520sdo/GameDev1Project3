c07e743dc5b93da7b02b78173a91c18f651f25b9
Jessica Lichter <jlichter123@gmail.com>
2018-11-05 12:31:39 -0500

    Working on child system

--- Assets/Scripts/ChildManager.cs
+++ Assets/Scripts/ChildManager.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChildManager : MonoBehaviour {
 
     public Transform child_name;
     public GameManager1 game_manager;
-    public Vector3 spawnPoint;
-    public Transform prevRoom;
-    public Transform[] available_rooms;
+    public Vector3 spawnPoint; //destination for child to move to 
+    public Transform prevRoom; //room that child was currently in 
+    public Transform nextRoom; //next room for child to go to 
+    public Transform[] available_rooms; //array of rooms available for child to go to 
+    public NavMeshAgent agent;
 
 
 
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+     void OnTriggerStay(Collider other) //to keep track of where the child already was 
+    {
+        if(other.gameObject.name == "LivingRoomTrigger")
+        {
+            prevRoom = available_rooms[0];
+        }
+        if (other.gameObject.name == "GirlRoomTrigger")
+        {
+            prevRoom = available_rooms[1];
+        }
+        if (other.gameObject.name == "BoysRoomTrigger")
+        {
+            prevRoom = available_rooms[2];
+        }
+        if (other.gameObject.name == "MomRoomTrigger")
+        {
+            prevRoom = available_rooms[3];
+        }
+        if (other.gameObject.name == "KitchenTrigger")
+        {
+            prevRoom = available_rooms[4];
+        }
+        if (other.gameObject.name == "BathroomTrigger")
+        {
+            prevRoom = available_rooms[5];
+        }
+
+    }
 }
--- Assets/Scripts/GameManager1.cs
+++ Assets/Scripts/GameManager1.cs
@@ -7,16 +7,31 @@ public class GameManager1 : MonoBehaviour {
     public Transform girl;
     public Transform bad_boy;
     public Transform odd_boy;
+
+    //for setting destinations/checking previous rooms 
+    public ChildManager girl_manager;
+    public ChildManager badb_manager;
+    public ChildManager oddb_manager;
+
+    Transform[] girlChoices;
+    Transform[] bad_boyChoices;
+    Transform[] mild_boyChoices;
+
     public float gameTime = 360; //game is 6 minutes long, so 360 seconds 
-    public Transform[] spawnPoints;
-    public GameObject[] kids;
-
-
+    public Transform[] spawnPoints; //array of places children can move to 
+    public GameObject[] kids; 
+    public ChildManager[] kidAgents; //for navMesh navigation 
+    bool start_game; 
+    
 	// Use this for initialization
 	void Start () {
 
 
-        SpawnChildren();
+        StartCoroutine(BeginGame());
+        if (start_game)
+        {
+
+        }
 
 
 	}
@@ -28,7 +43,12 @@ public class GameManager1 : MonoBehaviour {
         print(gameTime);
 
     }
-
+    IEnumerator BeginGame()
+    {
+        yield return new WaitForSeconds(3.0f);
+        start_game = true;
+    }
+    /*
     public void SpawnChildren()
     {
         List<Transform> freeSpawnPoints = new List<Transform>(spawnPoints);
@@ -41,6 +61,11 @@ public class GameManager1 : MonoBehaviour {
            // Instantiate(kids[i], pos.x, 0, pos.z, pos.rotation);
              Instantiate(kids[i], pos.position, pos.rotation);
         }
+    }
+    */
+    public void MoveChildren()
+    {
+      //  List<Transform> girl_destinations;
     }
 
 
